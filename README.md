b6467513f4e0f4948b7d86132f78a060032c8630
mounikasangana0126 <sssmounika5@gmail.com>
2023-11-11 17:25:09 +0530

    updated

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
 # webapi-currency-converter
- currency converter built with HTML, CSS, and JavaScript, providing real-time exchange rates using the APILayer API.
-<p>hjkjk</p>
-#gkjl
+
+# Currency Converter
+
+A versatile and feature-rich currency converter web application that allows users to quickly and accurately convert between different currencies. The project is built with HTML, CSS, and JavaScript, utilizing the [fixer.io](https://fixer.io/) API to fetch real-time exchange rates.
+
+## Features
+
+- **Real-Time Exchange Rates:** Utilizes the fixer.io API to provide up-to-date currency conversion rates.
+- **User-Friendly Interface:** Intuitive design for seamless and straightforward currency conversion.
+- **Dynamic Currency Selection:** Choose from a comprehensive list of currencies for both source and target conversions.
+- **Responsive Design:** Ensures a consistent and enjoyable user experience across various devices.
+
+## How to Use
+
+1. Open `index.html` in your web browser.
+2. Enter the amount in the "Enter Amount" field.
+3. Choose the source and target currencies from the dropdown menus.
+4. Click the "Convert" button to see the real-time converted amount.
+5. The result will be displayed below, showing the original amount, source currency, converted amount, and target currency.
+
+## Getting Started
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/currency-converter.git
+   cd currency-converter
+   ```
+
+2. Open the project in your favorite code editor to make any adjustments or enhancements.
+
+## Live Demo
+
+Explore the live [Currency Converter Demo](https://your-username.github.io/currency-converter/) for a hands-on experience.
+
+## Contributing
+
+Contributions are welcome! If you have suggestions, improvements, or bug fixes, please open an issue or submit a pull request.
+
+## Acknowledgments
+
+Special thanks to [fixer.io](https://fixer.io/) for providing the API that powers this currency converter.
+
+Feel free to customize and extend this project based on your needs. Happy converting! 💱🌐
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+
+  from_full = true;
+  to_full = true;
   // fetching values for "From" label
 
   document.getElementById("from").addEventListener("click", () => {
@@ -9,21 +11,25 @@ document.addEventListener("DOMContentLoaded", () => {
       method: 'GET',
       headers: myHeaders
     }
+    document.getElementById("result").style.display = "none";
   
-  fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
-  .then(response => response.json())
-    .then((result) => {
-      // fromSymbolValue = {};
-        for(i in result.symbols){
-          const node = document.createElement("option");
-          const node1 = document.getElementById("from").appendChild(node);
-          node1.innerHTML = i;
-          // fromSymbolValue[i] = result.symbols[i];
-          // return fromSymbolValue;
+    fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
+      .then(response => response.json())
+        .then((result) => {
+          if(from_full){
+          for (i in result.symbols) {
+            const node = document.createElement("option");
+            const node1 = document.getElementById("from").appendChild(node);
+            node1.innerHTML = i;
         }
+      }
+          from_full = false;
+          from_value = document.getElementById("from").value;
+          from_info = result.symbols[from_value];
+          document.getElementById("from_info").innerHTML =`${from_value} - ${from_info}`;
       }).catch(error => console.log('error', error));
   });
-  // console.log(fromSymbolValue);
+
   // fetching values for "To" label
 
   document.getElementById("to").addEventListener("click", () => {
@@ -33,18 +39,29 @@ document.addEventListener("DOMContentLoaded", () => {
       method: 'GET',
       headers: myHeaders
     }
+    document.getElementById("result").style.display = "none";
+    
   
-  fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
-  .then(response => response.json())
-    .then((result) => {
-      console.log(result);
-        for(i in result.symbols){
-          const node2 = document.createElement("option");
-          const node3 = document.getElementById("to").appendChild(node2);
-          node3.innerHTML = i;
+    fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
+      .then(response => response.json())
+        .then((result) => {
+          if (to_full) {
+          for (i in result.symbols) {
+            const node2 = document.createElement("option");
+            const node3 = document.getElementById("to").appendChild(node2);
+            node3.innerHTML = i;
         }
+      }
+        to_full = false;
+        to_value = document.getElementById("to").value;
+        to_info = result.symbols[to_value];
+        document.getElementById("to_info").innerHTML = `${to_value} - ${to_info}`;
     }).catch(error => console.log('error', error));
   });
+
+  document.getElementById("amount").addEventListener("click", () => {
+    document.getElementById("result").style.display = "none";
+})
 
   // fetching for the converted value
 
@@ -57,18 +74,21 @@ document.addEventListener("DOMContentLoaded", () => {
       method: 'GET',
       headers: myHeaders
     }
+    document.getElementById("result").style.display = "none";
 
     fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${To.value}&from=${From.value}&amount=${Amount.value}`, requestOptions)
       .then(response => response.json())
-      .then((result_object) => {
-        console.log(result_object);
-        if (Amount.value) {
-          document.getElementById("result_output").style.display = "revert";
-          document.getElementById("info").style.display = "revert";
-          document.getElementById("result").style.display = "revert";
-          document.getElementById("output").innerHTML = `${Amount.value} ${From.value} = <span style="color:yellow;font-weight:600;font-size:2rem">${result_object.result.toFixed(2)}</span> ${To.value}`;
+        .then((result_object) => {
+          if (Amount.value) {
+            document.getElementById("result_output").style.display = "revert";
+            document.getElementById("info").style.display = "revert";
+            document.getElementById("result").style.display = "revert";
+            document.getElementById("output").innerHTML = `${Amount.value} ${From.value} = <span style="color:yellow;font-weight:600;font-size:2rem">${result_object.result.toFixed(2)}</span> ${To.value}`;
         }
-  }).catch(error => console.log('error', error));
+          document.getElementById("time_stamp").innerHTML = `timestamp : ${result_object.info["timestamp"]}`;
+          document.getElementById("rate").innerHTML = `rate : ${result_object.info["rate"]}`;
+      }).catch(error => console.log('error', error));
+    
   });
 
   // fuction for the clear button to reset the values
@@ -83,7 +103,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // fuction for the info button to provide extra information
 
-  // document.getElementById("info").addEventListener("click", () => {
-    
-  // })
+  document.getElementById("info").addEventListener("click", () => {
+    document.getElementById("info_").style.display = "revert";
+    document.getElementById("info").style.display = "none";
+  })
+
+// fuction for the close button to close info
+  
+  document.getElementById("close").addEventListener("click", () => {
+    document.getElementById("info_").style.display = "none";
+    document.getElementById("info").style.display = "revert";
+  })
+
 })
