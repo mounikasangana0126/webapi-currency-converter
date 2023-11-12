5941addb3e97edaaa14b4581952a4f5468db1ab5
mounikasangana0126 <sssmounika5@gmail.com>
2023-11-12 15:14:40 +0530

    modified

--- main.js
+++ main.js
@@ -1,71 +1,61 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+  
+//fetching all currencies values
 
-  from_full = true;
-  to_full = true;
-  // fetching values for "From" label
+  async function currency_symbols() {
+    let myHeaders = new Headers();
+    myHeaders.append("apikey", "aP6V4hmchiK4B5gGh6FjpuFAfHq54AKe");
+    const requestOptions = {
+      method: 'GET',
+      headers: myHeaders
+    }
+    dict = {};
+  
+    response=await fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
+    result = await response.json();
+
+    for (i in result.symbols) {
+      const node = document.createElement("option");
+      const node1 = document.getElementById("from").appendChild(node);
+      node1.innerHTML = i;
+      dict[i] = result.symbols[i];
+    }
+    
+    for (i in result.symbols) {
+    const node2 = document.createElement("option");
+    const node3 = document.getElementById("to").appendChild(node2);
+    node3.innerHTML = i;
+    }
+    
+    return dict;
+  }
+  
+  //calling the api function
+  dictionary =await currency_symbols();
+ 
 
   document.getElementById("from").addEventListener("click", () => {
-    let myHeaders = new Headers();
-    myHeaders.append("apikey", "aP6V4hmchiK4B5gGh6FjpuFAfHq54AKe");
-    const requestOptions = {
-      method: 'GET',
-      headers: myHeaders
-    }
-    document.getElementById("result").style.display = "none";
-    document.getElementById("info_").style.display = "none";
-  
-    fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
-      .then(response => response.json())
-        .then((result) => {
-          if(from_full){
-          for (i in result.symbols) {
-            const node = document.createElement("option");
-            const node1 = document.getElementById("from").appendChild(node);
-            node1.innerHTML = i;
-        }
-      }
-          from_full = false;
-          from_value = document.getElementById("from").value;
-          from_info = result.symbols[from_value];
-          document.getElementById("from_info").innerHTML =`${from_value} - ${from_info}`;
-      }).catch(error => console.log('error', error));
-  });
-
-  // fetching values for "To" label
-
-  document.getElementById("to").addEventListener("click", () => {
-    let myHeaders = new Headers();
-    myHeaders.append("apikey", "aP6V4hmchiK4B5gGh6FjpuFAfHq54AKe");
-    const requestOptions = {
-      method: 'GET',
-      headers: myHeaders
-    }
-    document.getElementById("result").style.display = "none";
-    document.getElementById("info_").style.display = "none";
     
-  
-    fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
-      .then(response => response.json())
-        .then((result) => {
-          if (to_full) {
-          for (i in result.symbols) {
-            const node2 = document.createElement("option");
-            const node3 = document.getElementById("to").appendChild(node2);
-            node3.innerHTML = i;
-        }
-      }
-        to_full = false;
-        to_value = document.getElementById("to").value;
-        to_info = result.symbols[to_value];
-        document.getElementById("to_info").innerHTML = `${to_value} - ${to_info}`;
-    }).catch(error => console.log('error', error));
+    document.getElementById("result").style.display = "none";
+    document.getElementById("info_").style.display = "none";
+   
   });
+
+ 
+  document.getElementById("to").addEventListener("click", () => {
+
+    document.getElementById("result").style.display = "none";
+    document.getElementById("info_").style.display = "none";
+
+  });
+  
 
   document.getElementById("amount").addEventListener("click", () => {
     document.getElementById("result").style.display = "none";
-})
+    document.getElementById("info_").style.display = "none";
+  })
 
-  // fetching for the converted value
+  //fetching api to convert the selected currencies
 
   document.getElementById("currency-converter").addEventListener("submit", (event) => {
     event.preventDefault();
@@ -76,7 +66,6 @@ document.addEventListener("DOMContentLoaded", () => {
       method: 'GET',
       headers: myHeaders
     }
-    document.getElementById("result").style.display = "none";
     document.getElementById("info_").style.display = "none";
 
     fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${To.value}&from=${From.value}&amount=${Amount.value}`, requestOptions)
@@ -97,7 +86,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // fuction for the clear button to reset the values
 
   document.getElementById("clear").addEventListener("click", (event) => {
-    event.preventDefault();
+    
     document.getElementById("amount").value = "";
     document.getElementById("result").style.display = "none";
     document.getElementById("from").value = "";
@@ -107,13 +96,23 @@ document.addEventListener("DOMContentLoaded", () => {
 // fuction for the info button to provide extra information
 
   document.getElementById("info").addEventListener("click", () => {
+
     document.getElementById("info_").style.display = "revert";
     document.getElementById("info").style.display = "none";
+
+    from_value = document.getElementById("from").value;
+    from_info = dictionary[from_value];
+    document.getElementById("from_info").innerHTML = `${from_value} - ${from_info} `;
+
+    to_value = document.getElementById("to").value;
+    to_info = dictionary[to_value];
+    document.getElementById("to_info").innerHTML = `${to_value} - ${to_info}`;
   })
 
 // fuction for the close button to close info
   
   document.getElementById("close").addEventListener("click", () => {
+
     document.getElementById("info_").style.display = "none";
     document.getElementById("info").style.display = "revert";
   })
